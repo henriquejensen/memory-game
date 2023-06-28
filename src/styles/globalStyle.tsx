cac3de693fb455f855a477d5e1f955d8b3ece6f1
Henrique Jensen <henriquejensen@hotmail.com>
2023-06-28 17:57:08 -0300

    fix background color

--- src/styles/globalStyle.tsx
+++ src/styles/globalStyle.tsx
@@ -2,7 +2,7 @@ import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
   body {
-    background-color: #f1f1f1;
+    background-color: #FFF;
     font-family: 'Princess Sofia', cursive;
     font-size: 16px;
     margin: 0;
