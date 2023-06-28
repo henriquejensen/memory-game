2c3938408ddd44f11e0d6e7cf05137f2d0b37e0f
Henrique Jensen <henriquejensen@hotmail.com>
2023-06-28 15:29:21 -0300

    structuring the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,31 @@
 import './App.css'
+import first from './assets/18.svg'
 
 function App() {
   return (
     <>
-      <h1>Memory Game</h1>
+      <header>
+        <h1>Memory Game</h1>
+        <div>
+          <button>Restart</button>
+          <button>New Game</button>
+        </div>
+      </header>
+      <main>
+        <div>
+          <img src={first} alt="first" />
+        </div>
+      </main>
+      <footer>
+        <div>
+          <span>Time:</span>
+          <span>00:00</span>
+        </div>
+        <div>
+          <span>Movements:</span>
+          <span>100</span>
+        </div>
+      </footer>
     </>
   )
 }
